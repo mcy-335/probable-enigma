b7a400276bab41c94657ff74e54655820aaba3cb
MCY-335 <3088651257@qq.com>
2025-10-26 17:29:39 +0800

    代码优化

--- cpp-training/src/ExecutorImpl.hpp
+++ cpp-training/src/ExecutorImpl.hpp
@@ -12,53 +12,7 @@ ExecutorImpl& operator=(const ExecutorImpl&) = delete;
 public:
 void Execute(const std::string& command) noexcept override;
 Pose Query(void) const noexcept override;
-private:
-class ICommand{
-    public:
-    virtual ~ICommand()=default;
-    virtual void DoOperate(ExecutorImpl& executor) const noexcept=0;
-};
-class MoveCommand final:public ICommand
-{
 public:
-void DoOperate(ExecutorImpl& executor) const noexcept override
-{
-    if(executor.IsFast()){
-        executor.Move();
-    }
-    executor.Move();
-}
-};
-class TurnLeftCommand final:public ICommand
-{
-    public:
-    void DoOperate(ExecutorImpl& executor) const noexcept override
-    {
-        if(executor.IsFast()){
-        executor.Move();
-    }
-        executor.TurnLeft();
-    }
-};
-class TurnRightCommand final:public ICommand  
-{
-    public:
-    void DoOperate(ExecutorImpl& executor) const noexcept override
-    {
-        if(executor.IsFast()){
-        executor.Move();
-    }
-        executor.TurnRight();
-    }
-};
-class FastCommand final:public ICommand  
-{
-    public:
-    void DoOperate(ExecutorImpl& executor) const noexcept override
-    {
-        executor.Fast();
-    }
-};
 void Move(void) noexcept;
 void TurnLeft(void) noexcept;
 void TurnRight(void) noexcept;
