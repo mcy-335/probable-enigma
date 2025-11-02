c9b3d4b55cfa1126c9163a28aa3ba912a549e20d
MCY-335 <3088651257@qq.com>
2025-11-02 15:40:45 +0800

    std::map

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -1,6 +1,7 @@
 #include "ExecutorImpl.hpp"
 #include <memory>
 #include "Command.hpp"
+#include<unordered_map>
 namespace adas
 {
 Executor* Executor::NewExecutor(const Pose& pose) noexcept
@@ -12,26 +13,17 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
-    for(char cmd:commands){
-        std::unique_ptr<ICommand> cmder;
-        if(cmd == 'M'){
-            cmder = std::make_unique<MoveCommand>();
+    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+    cmderMap.emplace('M', std::make_unique<MoveCommand>());
+    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+    cmderMap.emplace('F', std::make_unique<FastCommand>());
+    for (const auto cmd : commands) {
+        const auto it = cmderMap.find(cmd);
+            if (it != cmderMap.end()) {
+                it->second->DoOperate(poseHandler);
+            }
         }
-        else if(cmd == 'L'){
-            cmder = std::make_unique<TurnLeftCommand>();
-        }
-        else if(cmd == 'R'){        
-            cmder = std::make_unique<TurnRightCommand>();
-        }
-        else if(cmd == 'F'){
-            cmder = std::make_unique<FastCommand>();
-        }
-        if(cmder){
-            cmder->DoOperate(poseHandler);
-        }
-    }
-    
-    
 }
 void ExecutorImpl::Move() noexcept
 {
